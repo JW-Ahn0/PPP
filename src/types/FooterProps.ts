09bac0ce7d6d2f0fc8347f76be6905de726eab07
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-09-26 14:13:20 +0900

    [Font] 폰트 추가

---
+++ src/types/FooterProps.ts
@@ -0,0 +1,8 @@
+export interface FooterProps {
+  /** Footer 저작권 관련 문구  */
+  copyRightText: string;
+}
+export interface FooterLogoListProps {
+  /** Footer 내에 로고 이미지 경로  */
+  imgList: string[];
+}
